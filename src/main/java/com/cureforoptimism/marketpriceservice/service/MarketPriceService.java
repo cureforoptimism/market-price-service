e304320317479966aa04605d5eff067711f00371
Cure For Optimism <cure_for_optimism@protonmail.com>
2022-06-19 16:35:13 -0600

    Log price updates

--- src/main/java/com/cureforoptimism/marketpriceservice/service/MarketPriceService.java
+++ src/main/java/com/cureforoptimism/marketpriceservice/service/MarketPriceService.java
@@ -2,6 +2,7 @@ package com.cureforoptimism.marketpriceservice.service;
 
 import com.cureforoptimism.marketpriceservice.domain.MarketPrice;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MarketPriceService {
   private final RedisTemplate<String, MarketPrice> redisTemplate;
 
@@ -19,7 +21,12 @@ public class MarketPriceService {
 
   @Scheduled(fixedDelay = 30000)
   public void publishMarketPrices() {
+    if(marketPrice.getPrice() == null) {
+      return;
+    }
+
     redisTemplate.convertAndSend(topic, marketPrice);
+    log.info("Market prices updated: " + marketPrice.getPrice());
   }
 
   public void setMagicPrice(Double magicPrice) {
